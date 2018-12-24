ea736823d217163623427f0962c92a3e1178f9a1
Ido <Bookidodo@gmail.com>
2018-12-24 21:00:58 +0200

    Updated DataReaderServer: samples and symbolTable*

--- DataReaderServer.cpp
+++ DataReaderServer.cpp
@@ -1,5 +1,183 @@
-//
-// Created by roy on 12/18/18.
-//
 
 #include "DataReaderServer.h"
+
+
+void DataReaderServer::set(string newPort, string newHerz){
+    cout << "start to set things.. " << stoi(newPort) << ", " << newHerz << "\n";
+    cout << "Before port is " << port << "\n";
+    port = stoi(newPort);
+    cout << "Then port is " << port << "\n";
+    herz = stoi(newHerz);
+    cout << "Done setting things\n";
+}
+
+bool DataReaderServer::isInBindMap(string subject) {
+    cout << "[DataReaderServer] Looking through bind map\n";
+//    cout << bindTable << endl;
+    if (bindTable.find(subject) == bindTable.end()) {
+        cout << "Wasn't found!\n";
+        return false;
+    }
+    return true;
+}
+
+double DataReaderServer::getFromBindValues(string bindVal){
+    cout << "[DataReader] Looking through the binds...\n";
+    return strobes.at(bindVal);
+}
+
+void DataReaderServer::setSock(int newSock){
+    sockfd = newSock;
+}
+
+int DataReaderServer::getSock(){
+    return sockfd;
+}
+
+void DataReaderServer::takeSamplesToTable(string parseMe) {
+    string delim = ",";
+    vector<string> sol;
+    //First step is to split the information
+    int start = 0;
+    int end = parseMe.find(delim);
+    while (end != std::string::npos) {
+        sol.push_back((parseMe.substr(start, end - start)));
+        start = end + delim.length();
+        end = parseMe.find(delim, start);
+    }
+    sol.push_back((parseMe.substr(start, end - start)));
+
+
+
+
+    //Second step is to put that to the map//
+    strobes["\"‫‪/instrumentation/airspeed-indicator/indicated-speed-kt\""] = stod(sol.at(0));
+    strobes["\"/instrumentation/altimeter/indicated-altitude-ft\""] = stod(sol.at(1));
+    strobes["\"‫‪/instrumentation/altimeter/pressure-alt-ft\""] = stod(sol.at(2));
+    strobes["‫‪\"/instrumentation/attitude-indicator/indicated-pitch-deg\""] = stod(sol.at(3));
+    strobes["\"‫‪/instrumentation/attitude-indicator/indicated-roll-deg\""] = stod(sol.at(4));
+    strobes["‫\"/instrumentation/attitude-indicator/internal-pitch-deg\""] = stod(sol.at(5));
+    strobes["\"/instrumentation/attitude-indicator/internal-roll-deg\""] = stod(sol.at(6));
+    strobes["‫‪\"/instrumentation/encoder/indicated-altitude-ft\""] = stod(sol.at(7));
+    strobes["‫‪\"/instrumentation/encoder/pressure-alt-ft\""] = stod(sol.at(8));
+    strobes["\"/instrumentation/gps/indicated-altitude-ft\""] = stod(sol.at(9));
+    strobes["\"‫‪/instrumentation/gps/indicated-ground-speed-kt\""] = stod(sol.at(10));
+    strobes["‫\"/instrumentation/gps/indicated-vertical-speed\""] = stod(sol.at(11));
+    strobes["‫‪\"/instrumentation/heading-indicator/indicated-heading-deg\""] = stod(sol.at(12));
+    strobes["\"/instrumentation/magnetic-compass/indicated-heading-deg\""] = stod(sol.at(13));
+    strobes["‫‪\"/instrumentation/slip-skid-ball/indicated-slip-skid\""] = stod(sol.at(14));
+    strobes["‫‪\"/instrumentation/turn-indicator/indicated-turn-rate\""] = stod(sol.at(15));
+    strobes["‫‪\"/instrumentation/vertical-speed-indicator/indicated-speed-fpm\""] = stod(sol.at(16));
+    strobes["\"/controls/flight/ailero\""] = stod(sol.at(17));
+    strobes["\"/controls/flight/elevator\""] = stod(sol.at(18));
+    strobes["‫‪\"/controls/flight/rudder\""] = stod(sol.at(19));
+    strobes["‫‪\"/controls/flight/flaps\""] = stod(sol.at(20));
+    strobes["‫\"/controls/engines/engine/throttle\""] = stod(sol.at(21));
+    strobes["‫‪\"/engines/engine/rpm\""] = stod(sol.at(22));
+
+
+    //ETC.. add all the addresses ...
+
+
+
+//    cout << "Lets see for example the rudder.." << strobes.at("‫‪/controls/flight/rudder‬‬") << "\n";
+//    printf("Done managing the rudder which is now.. %d\n", stod(strobes.at("‫‪/controls/flight/rudder‬‬")));
+}
+
+void DataReaderServer::updateBindedValues(){
+    cout << "[Update binded values] Welcome to check and update\n";
+    for (std::map<string, string>::iterator it = bindTable.begin(); it != bindTable.end(); ++it){
+        std::cout << it->first << " => " << it->second << '\n';
+        if (strobes[bindTable[it->first]] != 0){
+            cout << "Ok! there's a bineded value! Update it's symbolTable\n which is: " << it->first << endl;
+            symbolMap->set(it->first, strobes[bindTable[it->first]);
+        }
+        else{
+            strobes.erase(bindTable[it->first]);
+            bindTable.erase(it->first);
+        }
+    }
+
+}
+
+void DataReaderServer::setBind(string var, string address){
+    cout << "New var is binded as such: " << var << " = " << address << "\n";
+    bindTable[var] = address;
+}
+void * DataReaderServer::open() {
+    int sockfd, newsockfd, portno, clilen;
+    char buffer[256];
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
+
+    /* First call to socket() function */
+    setSock(socket(AF_INET, SOCK_STREAM, 0));
+
+    if (sockfd < 0) {
+        perror("ERROR opening socket");
+        exit(1);
+    }
+
+    /* Initialize socket structure */
+    bzero((char *) &serv_addr, sizeof (serv_addr));
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+
+    serv_addr.sin_port = htons(port);
+
+    /* Now bind the host address using bind() call.*/
+    if (bind(getSock(), (struct sockaddr *) &serv_addr, sizeof (serv_addr)) < 0) {
+        perror("ERROR on binding");
+        exit(1);
+    }
+
+    /* Now start listening for the clients, here process will
+     * go in sleep mode and will wait for the incoming connection
+     */
+
+    listen(getSock(), 5);
+    clilen = sizeof (cli_addr);
+    printf("Waiting for a client\n");
+    /* Accept actual connection from the client */
+    newsockfd = accept(getSock(), (struct sockaddr *) &cli_addr, (socklen_t*) & clilen);
+
+    if (newsockfd < 0) {
+        perror("ERROR on accept");
+        exit(1);
+    }
+    printf("Ever Listening..\n");       //SCAT! here you get array of information that updates the map..
+    /* If connection is established then start communicating */
+    while (true) {              //SCAT here
+        bzero(buffer, 256);
+        n = read(newsockfd, buffer, 255);
+
+        if (n < 0) {
+            perror("ERROR reading from socket");
+            exit(1);
+        }
+
+        printf("Information from simulator: %s\n", buffer);
+        string information = buffer;
+
+
+        takeSamplesToTable(information);   //receive and update local map.
+        updateBindedValues();
+
+
+//        printf("Got it : %c\n", information.substr(i, i = information.find(",")));
+//        while()
+//        strobes[]
+
+        /* Write a response to the client */
+//        n = write(newsockfd, "I got your message\n", 19);
+
+        if (n < 0) {
+            perror("ERROR writing to socket");
+            exit(1);
+        }
+    }
+}
+void DataReaderServer::close(){
+//    close();      //SCAT no close but hightly needed!
+}
--- DataReaderServer.h
+++ DataReaderServer.h
@@ -1,15 +1,51 @@
-//
-// Created by roy on 12/18/18.
-//
 
 #ifndef PROJECTPART1_DATAREADERSERVER_H
 #define PROJECTPART1_DATAREADERSERVER_H
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <netdb.h>
+#include <unistd.h>
+#include <netinet/in.h>
+
+#include <string.h>
+
+#include <sys/socket.h>
+
+#include <map>
+#include <vector>
+#include "SymbolTable.h"
+
+
+using namespace std;
 
 
 class DataReaderServer {
-
+map<string,double> strobes;     //first the address then the value
+map<string, string> bindTable;  //first the address then the variable
+SymbolTable* symbolMap; //SCAT! for now untill I know how to pass by pointer
+int sockfd;
+int port;
+int herz;
 public:
+    DataReaderServer() = default;
+    ~DataReaderServer() = default;
 
+    void set(string, string, SymbolTable*);
+    bool isInBindMap(string);
+    double getFromBindValues(string);
+    void takeSamplesToTable(string);
+    void setBind(string, string);
+    void updateBindedValues();
+    void * open();
+    static void * runOpen(void * a){
+      return ((DataReaderServer *)a)->open();
+    }
+    void close();
+
+    void setSock(int);
+    int getSock();
 };
 
 
